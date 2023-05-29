eae894e19b7a0d4e04a5b8c8ed53e1aa27e7035a
cristiamenciso <encisocristiam@gmail.com>
2023-05-29 17:00:16 -0400

    updated js and html to for search and login

--- js/code.js
+++ js/code.js
@@ -1,4 +1,4 @@
-const urlBase = 'http://COP4331-5.com/LAMPAPI';
+const urlBase = 'http://COPPARADISE.CLUB/LAMPAPI';
 const extension = 'php';
 
 let userId = 0;
@@ -139,47 +139,44 @@ function addColor()
 	
 }
 
-function searchColor()
-{
-	let srch = document.getElementById("searchText").value;
-	document.getElementById("colorSearchResult").innerHTML = "";
-	
-	let colorList = "";
+function searchColor() {
+  let search = document.getElementById("searchText").value;
+  document.getElementById("colorSearchResult").innerHTML = "";
 
-	let tmp = {search:srch,userId:userId};
-	let jsonPayload = JSON.stringify( tmp );
+  let contactList = "";
 
-	let url = urlBase + '/SearchColors.' + extension;
-	
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
-				let jsonObject = JSON.parse( xhr.responseText );
-				
-				for( let i=0; i<jsonObject.results.length; i++ )
-				{
-					colorList += jsonObject.results[i];
-					if( i < jsonObject.results.length - 1 )
-					{
-						colorList += "<br />\r\n";
-					}
-				}
-				
-				document.getElementsByTagName("p")[0].innerHTML = colorList;
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("colorSearchResult").innerHTML = err.message;
-	}
-	
+  let requestData = {
+    search: search,
+    userId: userId,
+  };
+  let jsonPayload = JSON.stringify(requestData);
+
+  let url = urlBase + '/SearchContacts.php';
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try {
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("colorSearchResult").innerHTML =
+          "Contact(s) have been retrieved";
+        let jsonResponse = JSON.parse(xhr.responseText);
+
+        for (let i = 0; i < jsonResponse.results.length; i++) {
+          let contact = jsonResponse.results[i];
+          contactList += "First Name: " + contact.FirstName + "<br>";
+          contactList += "Last Name: " + contact.LastName + "<br>";
+          contactList += "ID: " + contact.ID + "<br>";
+          contactList += "Phone: " + contact.Phone + "<br>";
+          contactList += "Email: " + contact.Email + "<br><br>";
+        }
+
+        document.getElementById("colorList").innerHTML = contactList;
+      }
+    };
+    xhr.send(jsonPayload);
+  } catch (err) {
+    document.getElementById("colorSearchResult").innerHTML = err.message;
+  }
 }
